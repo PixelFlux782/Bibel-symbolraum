7e554aba4e19ad6459ca9d2e7daa6157646f0281
PixelFlux782 <floriandalemans@gmail.com>
2026-05-30 21:33:39 +0200

    Feuer-SymbolRoom

---
+++ app/raeume/feuer/page.tsx
@@ -0,0 +1,10 @@
+import FireRoom from "@/components/rooms/fire/FireRoom";
+
+export const metadata = {
+  title: "Feuerraum",
+  description: "Ein Symbolraum zu \u05d0\u05e9 / Esh / Feuer.",
+};
+
+export default function FeuerRaumPage() {
+  return <FireRoom />;
+}
---
+++ components/rooms/fire/FireRoom.tsx
@@ -0,0 +1,6 @@
+import { SymbolRoom } from "../SymbolRoom";
+import { fireRoomDefinition } from "./fireRoomData";
+
+export default function FireRoom() {
+  return <SymbolRoom definition={fireRoomDefinition} />;
+}
